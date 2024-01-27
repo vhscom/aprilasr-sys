fac052cf98526dfc4e4b4fd020770f22daa075e5
vhs <vhsdev@tutanota.com>
2024-01-26 19:20:58 -0500

    feat: ✨ generate bindings from source

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! System crate for aprilasr speech-to-text library
+//!
+//! Documentation: [stable](https://docs.rs/aprilasr-sys/)
+
+/// Foreign Function Interface module
+#[allow(unused, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+pub mod ffi {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn it_can_initialize() -> Result<(), String> {
+        let result = unsafe { ffi::aam_api_init(1) };
+        assert_eq!(result, ());
+        Ok(())
     }
 }
